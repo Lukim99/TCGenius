4d0e4835add2a8e77a920384509cdd85cfe9f2b8
Lukim99 <casting.acc001@gmail.com>
2025-12-21 17:49:50 +0900

    인증서 무시

--- new_engine.js
+++ new_engine.js
@@ -138,6 +138,9 @@ async function doDcRecommend(targetUrl) {
             port: PROXY_CONFIG.port,
             auth: { username: proxyUser, password: PROXY_CONFIG.password }
         },
+        httpsAgent: new https.Agent({
+            rejectUnauthorized: false
+        }),
         timeout: 15000,
         headers: {
             'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 Mobile/15E148 Safari/604.1',
