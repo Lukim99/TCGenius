47dee6e55ea39848d6e89f9674c334b8cfee0fca
Lukim99 <casting.acc001@gmail.com>
2025-12-21 23:59:37 +0900

    test

--- new_engine.js
+++ new_engine.js
@@ -150,7 +150,7 @@ async function doDcAction(targetUrl, mode = 'normal') {
         'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 Mobile/15E148 Safari/604.1',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
         'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
-        'Referer': 'https://m.dcinside.com/'
+        'Referer': targetUrl
     };
 
     try {
