111d1cd81d2c3df5c04f448986d13fb893784b07
Lukim99 <casting.acc001@gmail.com>
2026-05-04 05:28:52 +0900

    fix

--- chatbot1.js
+++ chatbot1.js
@@ -5,7 +5,7 @@ const AWS = require('aws-sdk');
 AWS.config.update({
     region: 'ap-northeast-2',
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    secretAccessKey: process.env.AWS_SECRET_KEY_ID
 });
 
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
